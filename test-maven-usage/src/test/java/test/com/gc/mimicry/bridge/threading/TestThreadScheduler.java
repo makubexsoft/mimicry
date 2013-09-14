0798a54da2ae3f9a2a20129dbe5e4e34a56f9cfb
Marc-Christian Schulze <marc.christian.schulze@gmail.com>
2013-09-14 16:38:46 +0200

    some refactoring

--- test-maven-usage/src/test/java/test/com/gc/mimicry/bridge/threading/TestThreadScheduler.java
+++ test-maven-usage/src/test/java/test/com/gc/mimicry/bridge/threading/TestThreadScheduler.java
@@ -51,15 +51,6 @@ public class TestThreadScheduler
         Thread.currentThread().setContextClassLoader(loader);
 	}
 	
-	private void createStreams(UUID appId)
-	{
-		ConsoleInputStream stderr = ConsoleInputStream.attachStderr(broker, appId);
-        errReader = new BufferedReader(new InputStreamReader(stderr));
-        
-        ConsoleInputStream stdout = ConsoleInputStream.attachStdout(broker, appId);
-        outReader = new BufferedReader(new InputStreamReader(stdout));
-	}
-	
 	/**
 	 * Tests whether thread 1 is always executed before thread 2 and the number is incremented atomically 
 	 * using the {@link CheckpointBasedScheduler}.
@@ -69,73 +60,27 @@ public class TestThreadScheduler
     @Test
     public void testCheckpointsWithSyncBlocks() throws Exception
     {
-        Application app = Applications.create(ctx, new EntryPoint()
+    	runTest(new TestRunnable()
         {
+    		private int counter;
+    		
             @Override
-            public void main(String[] args)
+            public void run()
             {
-                final Object monitor = "My named Monitor";
-                final IntContainer container = new IntContainer();
-
-                Runnable target = new Runnable()
+                for (int i = 0; i < 300; ++i)
                 {
-                    @Override
-                    public void run()
+                    synchronized (this)
                     {
-                        for (int i = 0; i < 300; ++i)
-                        {
-                            synchronized (monitor)
-                            {
-                                System.out.println(Thread.currentThread().getName() + ":" + container.value++);
-                            }
-                        }
+                        System.out.println(Thread.currentThread().getName() + ":" + counter++);
                     }
-                };
-				Thread t1 = new Thread(target, "1");
-                Thread t2 = new Thread(target, "2");
-
-                t1.start();
-                t2.start();
-                
-                try
-				{
-                	// TODO: currently the main thread is not reported as RUNNING
-                	// to the thread scheduler which is why this doesn't deadlock.
-                	// We need to define the scheduling behavior for join operations.
-					t1.join();
-					t2.join();
-				}
-				catch ( InterruptedException e )
-				{
-					e.printStackTrace();
-				}
-
-                System.err.println(container.value);
+                }
             }
+
+			@Override
+			public int getCounter() {
+				return counter;
+			}
         });
-
-        createStreams(app.getId());
-        // TODO: this invocation of start should also be reported to the scheduler.
-        app.start(); 
-        app.getTerminationFuture().await( 3000 );
-        
-        // Test thread 1 is always executed before thread 2 and the number is incremented atomically
-        for(int i = 0; i < 600; i+=2)
-        {
-        	String line;
-        	String[] split;
-        	
-        	line = outReader.readLine();
-        	split = line.split( "\\:" );
-        	assertEquals( "1", split[0] );
-        	assertEquals( "" + i, split[1] );
-
-        	line = outReader.readLine();
-            split = line.split( "\\:" );
-            assertEquals( "2", split[0] );
-            assertEquals( "" + (i + 1), split[1] );
-        }
-        assertEquals("600", errReader.readLine());
     }
     
     /**
@@ -147,29 +92,44 @@ public class TestThreadScheduler
     @Test
     public void testCheckpointsWithSyncMethods() throws Exception
     {
-        Application app = Applications.create(ctx, new EntryPoint()
+        runTest(new TestRunnable()
+                {
+        			private int counter;
+        		
+                	 @Override
+                     public void run()
+                     {
+                         for (int i = 0; i < 300; ++i)
+                         {
+                            count();
+                         }
+                     }    
+                	 
+                     private synchronized void count()
+                     {
+                         System.out.println(Thread.currentThread().getName() + ":" + counter++);
+                     }
+
+					@Override
+					public int getCounter() {
+						return counter;
+					}
+                });
+    }
+    
+    @Test
+    public void testCheckpointsWithStaticSyncMethods() throws Exception
+    {
+    	runTest(new StaticMethodRunnable());
+    }
+    
+    private void runTest(final TestRunnable target) throws Exception
+    {
+    	Application app = Applications.create(ctx, new EntryPoint()
         {
             @Override
             public void main(String[] args)
             {
-                final IntContainer container = new IntContainer();
-
-                Runnable target = new Runnable()
-                {
-                    @Override
-                    public void run()
-                    {
-                        for (int i = 0; i < 300; ++i)
-                        {
-                           count();
-                        }
-                    }
-                    
-                    private synchronized void count()
-                    {
-                    	System.out.println(Thread.currentThread().getName() + ":" + container.value++);
-                    }
-                };
                 Thread t1 = new Thread(target, "1");
                 Thread t2 = new Thread(target, "2");
 
@@ -189,7 +149,7 @@ public class TestThreadScheduler
                     e.printStackTrace();
                 }
 
-                System.err.println(container.value);
+                System.err.println(target.getCounter());
             }
         });
 
@@ -217,64 +177,22 @@ public class TestThreadScheduler
         assertEquals("600", errReader.readLine());
     }
     
-    @Test
-    public void testCheckpointsWithStaticSyncMethods() throws Exception
-    {
-        Application app = Applications.create(ctx, new EntryPoint()
-        {
-            @Override
-            public void main(String[] args)
-            {
-                Runnable target = new StaticMethodRunnable();
-                Thread t1 = new Thread(target, "1");
-                Thread t2 = new Thread(target, "2");
-
-                t1.start();
-                t2.start();
-                
-                try
-                {
-                    // TODO: currently the main thread is not reported as RUNNING
-                    // to the thread scheduler which is why this doesn't deadlock.
-                    // We need to define the scheduling behavior for join operations.
-                    t1.join();
-                    t2.join();
-                }
-                catch ( InterruptedException e )
-                {
-                    e.printStackTrace();
-                }
-
-                System.err.println(StaticMethodRunnable.value);
-            }
-        });
-
-        createStreams(app.getId());
-        // TODO: this invocation of start should also be reported to the scheduler.
-        app.start(); 
-        app.getTerminationFuture().await( 3000 );
+    private void createStreams(UUID appId)
+	{
+		ConsoleInputStream stderr = ConsoleInputStream.attachStderr(broker, appId);
+        errReader = new BufferedReader(new InputStreamReader(stderr));
         
-        // Test thread 1 is always executed before thread 2 and the number is incremented atomically
-        for(int i = 0; i < 600; i+=2)
-        {
-            String line;
-            String[] split;
-            
-            line = outReader.readLine();
-            split = line.split( "\\:" );
-            assertEquals( "1", split[0] );
-            assertEquals( "" + i, split[1] );
-
-            line = outReader.readLine();
-            split = line.split( "\\:" );
-            assertEquals( "2", split[0] );
-            assertEquals( "" + (i + 1), split[1] );
-        }
-        assertEquals("600", errReader.readLine());
-    }
+        ConsoleInputStream stdout = ConsoleInputStream.attachStdout(broker, appId);
+        outReader = new BufferedReader(new InputStreamReader(stdout));
+	}
 }
 
-class StaticMethodRunnable implements Runnable
+interface TestRunnable extends Runnable
+{
+	public int getCounter();
+}
+
+class StaticMethodRunnable implements TestRunnable
 {
 	public static int value;
 	
@@ -291,6 +209,12 @@ class StaticMethodRunnable implements Runnable
     {
     	System.out.println(Thread.currentThread().getName() + ":" + value++);
     }
+
+	@Override
+	public int getCounter() {
+		// TODO Auto-generated method stub
+		return StaticMethodRunnable.value;
+	}
 }
 
 class IntContainer{
